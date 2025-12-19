36cd55de0fb308d9f131e79313308295567549b6
Ender1207 <hoangkimbao2007@gmail.com>
2025-12-19 17:34:30 +0700

    Fix double exit when play game

--- snake_game/snake/scenes/intro.py
+++ snake_game/snake/scenes/intro.py
@@ -141,6 +141,8 @@ class Menu:
                 if btn_Human.Click(event):
                     game = Play(self.screen)
                     game.Play_Game()
+                    pygame.mouse.set_pos((self.cfg.SCREEN_WIDTH, self.cfg.SCREEN_HEIGHT))
+                    pygame.event.clear()
                 
                 if btn_AI.Click(event):
                     pass
